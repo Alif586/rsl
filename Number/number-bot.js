8f1656a0a5f90c533df6b790b17a258a8abf1dc4
Alif586 <151447720+Alif586@users.noreply.github.com>
2026-01-15 12:14:07 +0600

    Update number-bot.js

--- Number/number-bot.js
+++ Number/number-bot.js
@@ -699,9 +699,9 @@ bot.on('message', async (msg) => {
                 
                 // 🔄 Git Pull & Restart
                 const { exec } = require('child_process');
-                const BOT_PATH = '/home/alif/sms';
+                const BOT_PATH = '/home/alif/rsl';
                 
-                exec(`cd ${BOT_PATH} && git reset --hard && git pull origin main && pm2 restart all`, (error, stdout, stderr) => {
+                exec(`cd ${BOT_PATH} && git reset --hard && git pull origin main && pm2 restart rsl`, (error, stdout, stderr) => {
                     if (error) {
                         bot.sendMessage(chatId, `❌ **Restart Failed!**\n\n<pre>${error.message}</pre>`, { 
                             parse_mode: 'HTML',
